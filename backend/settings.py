9879ef883c9bdd91663fc82dee6651a9b733ed0d
vikassnwl <vikassnwl@gmail.com>
2021-03-10 13:18:02 +0530

    changes made

--- backend/settings.py
+++ backend/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../frontend')],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '../frontend/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
